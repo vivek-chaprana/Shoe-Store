ab49d655f0493fad86b5e5134be639cdf30683e0
vivek-chaprana <vivek2003ji@gmail.com>
2023-07-26 10:54:27 +0530

    Add: First Commit

---
+++ Frontend/utils/urls.js
@@ -0,0 +1,7 @@
+// SECRET TOKEN FOR STRAPI API CALL
+export const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
+
+// STRAPI API URL
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://shoe-store-backend-vicky.onrender.com";
